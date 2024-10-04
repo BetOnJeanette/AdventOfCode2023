4f94b3ce858126b0f9909b21dea2160ec49a88e8
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-10-03 23:36:15 -0400

    Move file reading to core

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,7 +1,9 @@
+use std::fs;
 mod part1;
+mod part2;
 
 fn main(){
-    let input_file = "../input.txt";
+    let input_file = fs::read_to_string("../input.txt").unwrap();
     let first_result = part1::get_result(input_file);
     println!("part 1: {first_result}")
 }
