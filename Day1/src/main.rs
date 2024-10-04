3be784878e861ea551d937a5cd618e6409242641
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-10-03 23:28:43 -0400

    Solve day 1

---
+++ Day1/src/main.rs
@@ -0,0 +1,7 @@
+mod part1;
+
+fn main(){
+    let input_file = "../input.txt";
+    let first_result = part1::get_result(input_file);
+    println!("part 1: {first_result}")
+}
---
+++ Day1/src/part1.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn get_line_num(line: &str) -> u32 {
+    let first_digit = (line.as_bytes()[line.find(char::is_numeric).unwrap()] as char).to_digit(10).unwrap();
+    let last_digit = (line.as_bytes()[line.rfind(char::is_numeric).unwrap()] as char).to_digit(10).unwrap();
+    return 10 * first_digit + last_digit;
+}
+
+pub fn get_result(file: &str) -> String {
+    let sum = fs::read_to_string(file).unwrap().lines().fold(0, |acc: u32, val: &str| acc + get_line_num(val));
+    return sum.to_string();
+}
