cdab379245082a64545e3debf4a942d49c97d384
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-10-04 16:50:28 -0400

    Complete part 2

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -1,8 +1,11 @@
 use std::fs;
 mod part1;
+mod part2;
 
 fn main(){
     let input_file = fs::read_to_string("../input.txt").unwrap();
     let first_result = part1::get_result(&input_file);
+    let second_result = part2::get_result(&input_file);
     println!("part 1: {first_result}");
+    println!("part 2: {second_result}")
 }
